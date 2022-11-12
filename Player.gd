51805b35ef47733647afecd1825aed5b5f559136
swoods45 <57550537+SomeSortOfSam@users.noreply.github.com>
2022-11-12 11:32:30 -0600

    Tweened player movement

--- Player.gd
+++ Player.gd
@@ -6,16 +6,16 @@ const WIN_TILE_ID := 3
 
 @onready var map : TileMap = $".."
 var current_cell : Vector2i
-var _is_moving : bool
+var tween : Tween
 
 signal won
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input : Vector2i = Vector2i(Input.get_axis("ui_left","ui_right"), Input.get_axis("ui_up","ui_down"))
-	if !_is_moving && input != Vector2i.ZERO && map.get_cell_source_id(0,current_cell + input) not in WALL_TILE_IDS:
-		print(input,current_cell)
+	if (!tween || !tween.is_running()) && input != Vector2i.ZERO && map.get_cell_source_id(0,current_cell + input) not in WALL_TILE_IDS:
 		current_cell += input
-		translate(input * map.tile_set.tile_size)
+		tween = create_tween()
+		tween.tween_property(self,"position",position + input * map.tile_set.tile_size * 1.0,.2)
 		if map.get_cell_source_id(0,current_cell) == WIN_TILE_ID:
 			emit_signal("won")
