864d783b2371b94b9de5d6eda21f81fe40abbb82
SomeSortOfSam <57550537+SomeSortOfSam@users.noreply.github.com>
2022-11-13 10:00:03 -0600

    Created level transitions

--- Level.gd
+++ Level.gd
@@ -10,30 +10,42 @@ const WALL_CELL_ID := 0
 @export var on_color : Color
 
 @onready var restart_label : Label = $"CanvasLayer/Restart Text"
+@onready var camera : Camera2D = $Camera2D
 
 var camera_list := []
 var ping : GPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_utility_objects()
+	modulate = Color(1,1,1,0)
+	start_camera_change()
 	spawn_wall_objects()
+	spawn_utility_objects()
+	@warning_ignore(return_value_discarded)
+	create_tween().tween_property(self,"modulate",Color.WHITE,1.5).set_ease(Tween.EASE_IN)
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_restart"):
 		@warning_ignore(return_value_discarded)
 		get_tree().reload_current_scene()
 
+func start_camera_change():
+	@warning_ignore(shadowed_variable)
+	var camera = get_node_or_null("../Camera2D")
+	if !camera:
+		remove_child(self.camera)
+		$"..".call_deferred("add_child",self.camera)
+		camera = self.camera
+	@warning_ignore(return_value_discarded)
+	create_tween().tween_property(camera,"position",self.camera.position,1.5)
+	if camera != self.camera:
+		self.camera.queue_free()
+
 func spawn_utility_objects():
-	var player : Player
 	for cell in get_used_cells(0):
 		var cell_id = get_cell_source_id(0,cell)
 		if cell_id == PLAYER_START_CELL_ID:
-			player = spawn_player(cell)
-			@warning_ignore(return_value_discarded)
-			player.connect("won",_on_player_win)
-			@warning_ignore(return_value_discarded)
-			player.connect("lost",_on_player_lose)
+			spawn_player(cell)
 			erase_cell(0,cell)
 
 func spawn_wall_objects():
@@ -45,13 +57,20 @@ func spawn_wall_objects():
 			spawn_occluder(cell)
 			get_cell_tile_data(1,cell).modulate = Color.DARK_GRAY
 
-func spawn_player(cell : Vector2i) -> Player:
-	var player : Player = player_scene.instantiate()
-	player.position = cell * tile_set.tile_size * 1.0 + tile_set.tile_size/2.0
-	player.top_level = true
-	player.current_cell = cell
-	add_child(player)
-	return player
+func spawn_player(cell : Vector2i):
+	var player : Player = get_node_or_null("../Player")
+	if !player:
+		player = player_scene.instantiate()
+		$"..".call_deferred("add_child",player)
+		player.call_deferred("reset", cell, self)
+	else:
+		player.reset(cell, self)
+	@warning_ignore(return_value_discarded)
+	player.connect("won",_on_player_win)
+	@warning_ignore(return_value_discarded)
+	player.connect("lost",_on_player_lose)
+	@warning_ignore(return_value_discarded)
+	player.connect("saw_exit",_on_player_saw_exit)
 
 func spawn_occluder(cell : Vector2i):
 	var occluder : StaticBody2D = occluder_scene.instantiate()
@@ -65,12 +84,21 @@ func spawn_ping(cell : Vector2i):
 	add_child(ping)
 
 func _on_player_win():
+	await get_tree().process_frame
+	var tween = create_tween()
+	@warning_ignore(return_value_discarded)
+	tween.tween_property(self,"modulate",Color(1,1,1,0),1).set_ease(Tween.EASE_OUT)
+	await tween.finished
 	@warning_ignore(return_value_discarded)
 	get_tree().change_scene_to_packed(next_level)
 
 func _on_player_lose():
 	ping.visible = false
+	await get_tree().process_frame
+	@warning_ignore(return_value_discarded)
 	create_tween().tween_property(restart_label,"modulate", Color.WHITE,3)
+	@warning_ignore(return_value_discarded)
+	create_tween().tween_property(self,"modulate",Color(1,1,1,0),1.5).set_ease(Tween.EASE_OUT)
 
 func _on_player_saw_exit():
 	ping.visible = false
--- Player.gd
+++ Player.gd
@@ -7,9 +7,10 @@ const WALL_EXCEPTIONS := [-1,WIN_TILE_ID]
 const OCCLUDER_EXCEPTIONS := [GLASS_TILE_ID,WIN_TILE_ID]
 const MAX_VOID := 5
 
-@onready var map : TileMap = $".."
 @onready var raycast : RayCast2D = $RayCast2D
+@onready var light : PointLight2D = $PointLight2D
 
+var map : TileMap
 var current_cell : Vector2i
 var tween : Tween
 var previous_direction : Vector2i
@@ -21,13 +22,29 @@ signal lost
 signal saw_exit
 
 func _process(_delta):
-	if do_void_check() && _handle_inputs():
+	if map && (do_void_check() || _handle_inputs()):
 		await tween.finished
-		recaculate_line_of_sight()
+		if map:
+			recaculate_line_of_sight()
+
+func reset(current_cell : Vector2i, map : TileMap):
+	in_sight_set = PackedVector2Array()
+	void_count = 0
+	previous_direction = Vector2i.ZERO
+	if tween:
+		tween.stop()
+	light.energy = 0
+	create_tween().tween_property(light,"energy",1,.2)
+	self.current_cell = current_cell
+	self.map = map
+	tween = create_tween()
+	tween.tween_property(self,\
+	"position",self.current_cell * map.tile_set.tile_size * 1.0 + map.tile_set.tile_size/2.0,.4)\
+	.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 
 func do_void_check() -> bool:
 	if void_count > MAX_VOID:
-		return false
+		return true
 	modulate = lerp(Color.WHITE,Color(1,1,1,0),void_count/MAX_VOID)
 	var current_cell_id = map.get_cell_source_id(2,current_cell)
 	if current_cell_id != -1:
@@ -40,8 +57,8 @@ func do_void_check() -> bool:
 		if void_count > MAX_VOID:
 			@warning_ignore(return_value_discarded)
 			emit_signal("lost")
-			queue_free()
-	return true
+		return true
+	return false
 
 func _handle_inputs() -> bool:
 	@warning_ignore(narrowing_conversion)
@@ -60,6 +77,7 @@ func move_in_direction(direction : Vector2i):
 	var _tween = tween.tween_property(self,"position",position + direction * map.tile_set.tile_size * 1.0,.2)
 	previous_direction = direction
 	if map.get_cell_source_id(1,current_cell) == WIN_TILE_ID:
+		await tween.finished
 		var _signal = emit_signal("won")
 
 func recaculate_line_of_sight():
@@ -74,7 +92,8 @@ func recaculate_line_of_sight():
 			if cell_id not in OCCLUDER_EXCEPTIONS:
 				map.get_cell_tile_data(1,cell).modulate = map.on_color
 		elif Vector2(cell) in in_sight_set:
-			destory_cell(cell)
+			if !destory_cell(cell):
+				break
 
 func get_is_visible(cell : Vector2i) -> bool:
 	for target_position in [to_local(cell * map.tile_set.tile_size + Vector2i.ONE * 4),
@@ -91,13 +110,25 @@ func get_is_visible(cell : Vector2i) -> bool:
 				return true
 	return false
 
-func destory_cell(cell : Vector2i):
-	if map.get_cell_source_id(1,cell) == WIN_TILE_ID:
+func destory_cell(cell : Vector2i) -> bool:
+	var cell_id = map.get_cell_source_id(1,cell)
+	map.erase_cell(1,cell)
+	if cell_id == WIN_TILE_ID:
 		@warning_ignore(return_value_discarded)
 		emit_signal("lost")
-	map.erase_cell(1,cell)
+		return false
 	var node = map.get_node_or_null(str(cell))
 	if node:
 		node.queue_free()
 	map.set_cells_terrain_connect(2,[cell],0,2)
 	map.force_update(2)
+	return true
+
+func _on_won():
+	await get_tree().process_frame
+	map = null
+
+func _on_lost():
+	create_tween().tween_property(light,"energy",0,1)
+	await get_tree().process_frame
+	map = null
